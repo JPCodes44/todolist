8dd5204f523f83d79b7157b2f2fc1cbec75fa02a
JPCodes44 <jp.mak44@gmail.com>
2025-05-13 14:04:49 +0200

    made app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import {TodoForm} from './assets/components/todoform'
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <TodoForm />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/assets/components/todoitem.tsx
@@ -0,0 +1,11 @@
+// TodoItem – shows a single task, with “done/undone” toggle and delete button
+
+import React from 'react';
+import App from '../../App'
+import ReactDOM from 'react-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root')!)
+
+export const TodoItem = () => {
+    
+}
